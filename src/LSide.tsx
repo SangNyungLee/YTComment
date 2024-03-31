1dbe10a6aa7e4a18329013cdcb6c7cb44f659c49
SangNyungLee <lsy6747@gmail.com>
2024-04-01 03:11:13 +0900

    로그인 이벤트 추가

--- src/LSide.tsx
+++ src/LSide.tsx
@@ -2,17 +2,41 @@ import React, { useEffect, useState } from "react";
 import "./css/LsideStyle.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AccordionFlush from "./Accordion";
-import { GoCommentDiscussion } from "react-icons/go";
 import { BsFillBarChartLineFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { deleteCookie } from "./func/GetApi";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { getCookie } from "./func/GetApi";
+
 export default function Lside() {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
   const cookieString = document.cookie;
   const userName = sessionStorage.getItem("userName");
   const toggleMenu = () => {
     setIsExpanded(!isExpanded);
+  };
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key === "Enter") {
+      getLogin();
+    }
+  };
+  //로그인 하는 함수
+  const getLogin = async () => {
+    const result: any = await axios.post("http://localhost:8000/login", {
+      userId,
+      userPw,
+    });
+    if (result.data === "fail") {
+      alert("아이디와 비밀번호를 확인해주세요");
+      setUserId("");
+      setUserPw("");
+    } else {
+      getCookie("token", result.data.token);
+      sessionStorage.setItem("userName", result.data.user.username);
+      alert("로그인에 성공하셨습니다.!");
+    }
   };
   const logout = () => {
     deleteCookie();
@@ -30,18 +54,40 @@ export default function Lside() {
       </div>
       {cookieString ? (
         <div>
-          <div>{userName} 님 환영합니다.~</div>
-          <button onClick={logout}>로그아웃하기</button>
+          <div className="welcome">
+            <span style={{ color: "black", fontWeight: "700" }}>
+              {userName}
+            </span>{" "}
+            님 환영합니다.~
+          </div>
+          <button className="btnLogin" onClick={logout}>
+            로그아웃
+          </button>
         </div>
       ) : (
         <div>
-          <input type="text" className="idInput" placeholder="아이디"></input>
-          <input type="text" className="idInput" placeholder="비밀번호"></input>
-          <button className="btnLogin">로그인</button>
+          <input
+            type="text"
+            value={userId}
+            className="idInput"
+            placeholder="아이디"
+            onChange={(e) => setUserId(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            value={userPw}
+            className="idInput"
+            placeholder="비밀번호"
+            onChange={(e) => setUserPw(e.target.value)}
+            onKeyDown={handleKeyPress}
+          ></input>
+          <button className="btnLogin" onClick={getLogin}>
+            로그인
+          </button>
           <br />
-          <div className="autoSign">
-            <input type="checkbox" className="loginCheckbox" />
-            <span className="autoLogin">자동로그인</span>
+          <div className="autoSign" style={{ textAlign: "center" }}>
+            {/* <input type="checkbox" className="loginCheckbox" />
+            <span className="autoLogin">자동로그인</span> */}
             <span className="signup">
               <span>
                 <Link
--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "./css/Signup.css";
 import axios from "axios";
-import { useDispatch } from "react-redux";
 import {
-  FacebookLoginButton,
   GithubLoginButton,
   GoogleLoginButton,
 } from "react-social-login-buttons";
@@ -14,7 +12,6 @@ import { getCookie } from "./func/GetApi";
 function Signup() {
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
-  const dispatch = useDispatch();
   const getLogin = async () => {
     const result: any = await axios.post("http://localhost:8000/login", {
       userId,
@@ -30,6 +27,11 @@ function Signup() {
       alert("로그인에 성공하셨습니다.!");
     }
   };
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      getLogin();
+    }
+  };
   return (
     <div className="mom">
       <div className="login">
@@ -43,7 +45,7 @@ function Signup() {
           <h1 style={{ color: "#4d627b", fontSize: "2em" }}>로그인</h1>
         </div>
         <div className="loginForm">
-          <form className="formTag">
+          <div className="formTag">
             <input
               type="text"
               value={userId}
@@ -61,6 +63,7 @@ function Signup() {
               onChange={(e) => {
                 setUserPw(e.target.value);
               }}
+              onKeyDown={handleKeyPress}
             />
             <button
               type="button"
@@ -85,11 +88,11 @@ function Signup() {
                 회원가입
               </button>
             </Link>
-          </form>
+          </div>
         </div>
-        <div className="dddd" style={{}}>
-          <input type="checkBox" style={{ marginRight: "10px" }} />
-          <span>자동로그인</span>
+        <div className="dddd">
+          {/* <input type="checkBox" style={{ marginRight: "10px" }} />
+          <span>자동로그인</span> */}
         </div>
         <div className="socialLogin">
           <div className="loginSNS">SNS 계정으로 로그인</div>
