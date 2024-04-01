2952aa9f30fb6e3eb59a10263f8fb0d87704c04e
SangNyungLee <lsy6747@gmail.com>
2024-04-02 02:35:32 +0900

    server 추가, api주소 수정

---
+++ server/routes/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import controller from "../controller/index";
+const router = express.Router();
+
+router.get("/getVideos", controller.getVideos); //유튜브 api로 데이터 받아오는거
+router.post("/trending", controller.trend); //인기순
+router.post("/getComments", controller.getComments);
+router.post("/getCount", controller.getCount);
+router.post("/checkUserEmail", controller.checkUserEmail); //이메일 중복확인
+router.post("/userSignup", controller.userSignup); //회원가입
+router.post("/auth/kakao", controller.kakao);
+router.post("/login", controller.login);
+router.get("/totalPage", controller.totalPage);
+export default router;
---
+++ server/server.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from "express";
+import router from "./routes/index";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app: Express = express();
+const PORT: Number = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+//JSON 형식 POST 데이터 파싱하기 위한 미들웨어 추가
+app.use(express.json());
+// 라우터 설정
+app.use("/api", router);
+
+app.get("*", (req: Request, res: Response) => {
+  res.render("404");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
